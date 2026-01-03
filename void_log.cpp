e71b060e9f2bb88cd32591324f5016af6a60e8d1
igor y <yakobchuk.igor@yahoo.com>
2026-01-03 17:16:00 +0200

    remove duplicate source files froom root...kepp day01 structure

--- void_log.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void Log( const char* message) {      //2.создал ф-цию Log вместо cout которая принимает строку как параметр и выведем сообщение в консоль
-//  3.const char*  - это у нас тип данных который может содержать строку текста
-
-
-    std::cout << message << std::endl;
-
-}
