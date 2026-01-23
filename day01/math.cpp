c14fec99f3379f8892f8886c4d826349f4eef0bb
igor y <yakobchuk.igor@yahoo.com>
2026-01-23 17:05:56 +0200

    разобрался с компиляцией доп файлов #include ''EndBrace.h'', макросом-константой define и как он делает текстовую подстановку и операторами #if 1, #if 0, #endif который отключает/включает код

---
+++ day01/math.cpp
@@ -0,0 +1,14 @@
+
+#if 1 //другими словами это означает true
+
+// если я напишу if 0 и в конце endif то это отключет наш код 
+
+//это ещё одна фича оператора 
+
+int Multiplay (int a, int b) 
+{
+    int result = a * b;
+    return result;
+
+}
+#endif 
