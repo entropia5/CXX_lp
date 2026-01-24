994317d53c77db6588c3dd9dcadb0b71c91e38b8
igor y <yakobchuk.igor@yahoo.com>
2026-01-24 04:37:41 +0200

    разделил ф-цию math на 2 исходника...на math.cpp и log.cpp и вручную скомпилировал, скомпоновал и создал исполняемый файл math_log

--- day02/math.cpp
+++ day02/math.cpp
@@ -3,14 +3,7 @@
 
 #include <iostream>
 
-void Log( const char* message) {     
-    
-     //2.создал ф-цию Log вместо cout которая принимает строку как параметр и выведем сообщение в консоль
-//  3.const char*  - это у нас тип данных который может содержать строку текста
-
-
-    std::cout << message << std::endl;
-}
+void Log( const char* message); //добавил объявление функции log 
 
 int Multiply (int a, int b) 
 {
