7f67ce19496404ca1171f8b5c021414fa2ea0dd7
igor y <yakobchuk.igor@yahoo.com>
2026-01-30 00:05:16 +0200

    update

---
+++ day04/functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int multiply (int a, int b) 
+{
+    return a * b;  
+}
+
+int main()
+{
+
+int result = multiply (3, 3);
+
+std::cout << result << std :: endl;
+std :: cin.get();
+
+
+return 0;
+
+
+}
