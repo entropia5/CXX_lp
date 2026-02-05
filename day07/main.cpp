eafa70b97b467a4d348867e9f68914f16c324584
igor y <yakobchuk.igor@yahoo.com>
2026-02-05 07:37:31 +0200

    if else if else practice

--- day07/main.cpp
+++ day07/main.cpp
@@ -5,10 +5,18 @@
 int main () 
 {
 
-int x = 5;
+//если у нас есть указатель
 
-if (x == 5)
- Log("Hello Debug !");
+const char* ptr = "Hello ";
+
+if (ptr) 
+ Log(ptr);
+
+else if (ptr == "Hello")
+    Log("Ptr Hello");
+
+ else 
+    Log("Ptr is null!");
 
     
     std::cin.get();
